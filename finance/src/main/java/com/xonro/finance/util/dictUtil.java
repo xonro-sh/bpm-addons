285db56f25807c0da548a2200ab1c443414158f4
TheLionKing <35064272+XonroH@users.noreply.github.com>
2018-01-25 15:07:10 +0800

    数据字典公共类

---
+++ finance/src/main/java/com/xonro/finance/util/dictUtil.java
@@ -0,0 +1,25 @@
+package com.xonro.finance.util;
+
+/**
+ * 字典公共类
+ * @author  haolh
+ * @date  2018/1/25.
+ */
+public class dictUtil {
+    /**
+     * 根据一级科目编号获取一级科目名称
+     * @param firSubjectNo
+     * @return
+     */
+    public static String getFirSubjectName(String firSubjectNo){
+        return "";
+    }
+    /**
+     * 根据二级科目编号获取二级科目名称
+     * @param secSsubjectNo
+     * @return
+     */
+    public static String getSecSubjectName(String secSsubjectNo){
+        return "";
+    }
+}
