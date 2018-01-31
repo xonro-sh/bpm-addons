73af7aa28511fa30d9119f37c8fcbd671cc8b14d
TheLionKing <35064272+XonroH@users.noreply.github.com>
2018-01-31 13:32:44 +0800

    获取预算汇总数据处理类

--- finance/src/main/java/com/xonro/finance/util/dictUtil.java
+++ finance/src/main/java/com/xonro/finance/util/dictUtil.java
@@ -1,6 +1,15 @@
 package com.xonro.finance.util;
 
 import com.actionsoft.bpms.util.DBSql;
+import com.alibaba.fastjson.JSON;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 字典公共类
@@ -25,5 +34,36 @@ public class dictUtil {
     public static String getSecSubjectName(String secSubjectNo){
         return DBSql.getString("select CNNAME from BO_ACT_DICT_KV_ITEM " +
                 "where DICTKEY='FINANCE.SUBJECT' and ITEMNO='"+secSubjectNo+"'");
+    }
+    /**
+     * 获取二级科目及科目名称
+     * @return
+     */
+    public static String getSecSubjects(){
+        Connection conn = DBSql.open();
+        Statement stat = null;
+        ResultSet result = null;
+
+        //查询所有二级科目
+        String sql = "select ITEMNO,CNNAME from BO_ACT_DICT_KV_ITEM where DICTKEY='FINANCE.SUBJECT' and EXTTEXT1!='' and EXTTEXT3=''";
+
+        //新建科目集合
+        List<Map> subjectList = new ArrayList<Map>();
+        try{
+            stat = conn.createStatement();
+            result = stat.executeQuery(sql.toString());
+            //从查询结果集 生成集合
+            while (result.next()) {
+                Map map = new HashMap();
+                map.put("ITEMNO",result.getString("ITEMNO"));
+                map.put("CNNAME",result.getString("CNNAME"));
+                subjectList.add(map);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }finally {
+            DBSql.close(conn);
+        }
+        return JSON.toJSONString(subjectList);
     }
 }
